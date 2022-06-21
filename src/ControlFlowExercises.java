1049ea159672e4c3f4fc363274a726835eb0125c
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-06-21 15:55:54 -0500

    updating repo

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,61 @@
+package src;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // While Loop
+
+//        int i = 5;
+//        while (i <= 15) {
+//          System.out.println(i + " ");
+//          i++;
+//        };
+
+        // Do While Loop
+
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
+
+        //Count backwards by 5
+
+//        int i = 100;
+//        do {
+//          System.out.println(i);
+//          i -= 5;
+//        } while (i >= -10);
+
+        // Do While to 1mil!
+
+//        long atTwo = 2;
+//        do {
+//            System.out.println(atTwo);
+//            atTwo *= atTwo;
+//        } while (atTwo <= 1000000);
+
+        // While loop but with For loop instead
+
+//        for (int i = 5; i <= 15; i+=1) {
+//            System.out.println(i);
+//        };
+
+        // Do While Loop counting to 100
+
+//        for (int i = 2; i <= 100; i += 2) {
+//            System.out.println(i);
+//        }
+
+        // Do While Loop counting backwards to -10
+
+//        for (int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+
+        // Do While up 1mil but with For Loop
+
+//        for (long i = 2; i <= 1000000; i *= i){
+//            System.out.println(i);
+//        };
+    }
+}
