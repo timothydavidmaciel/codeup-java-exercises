113511b90fd65eff65d19e468b400f5a4774114b
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-06-22 10:35:40 -0500

    updating repo

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,10 @@
 package src;
+import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         // While Loop
 
 //        int i = 5;
@@ -57,5 +60,65 @@ public class ControlFlowExercises {
 //        for (long i = 2; i <= 1000000; i *= i){
 //            System.out.println(i);
 //        };
+
+        // FizzBuzz
+
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 15 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+//        // The Table of Power!!!
+//
+//        boolean theWay = true;
+//
+//        while(theWay) {
+//            System.out.println("Please enter a number.");
+//            int userInput = scanner.nextInt();
+//            String tableSetup = "Here is you table!\n\nnumber | squared | cubed\n------ | ------- | -----";
+//            System.out.println(tableSetup);
+//            for (long table = 1; table <= userInput; table++) {
+//                long squared = table * table;
+//                long cubed = table * table * table;
+//                System.out.printf("%-6d | %-7d | %-5d %n", table, squared, cubed);
+//            }
+//            System.out.println("Do you want to enter another number? (Y/N)");
+//            String userChoice = scanner.next();
+//            if (userChoice.equalsIgnoreCase("n")) {
+//                theWay = false;
+//            }
+//        }
+
+        // Grades!!!!!!
+
+        boolean decision = true;
+
+        while (decision) {
+            System.out.println("Please enter your grade (0 - 100)");
+            int userGrade = scanner.nextInt();
+            if (userGrade > 0 && userGrade <= 59) {
+                System.out.println('F');
+            } else if (userGrade > 59 && userGrade <= 66) {
+                System.out.println('D');
+            } else if (userGrade > 66 && userGrade <= 79) {
+                System.out.println('C');
+            } else if (userGrade > 79 && userGrade <= 87) {
+                System.out.println('B');
+            } else if (userGrade > 87 && userGrade <= 100) {
+                System.out.println('A');
+            }
+            System.out.println("Do you want to enter another grade? (Y/N)");
+            String userChoice = scanner.next();
+            if(userChoice.equalsIgnoreCase("n")) {
+                decision = false;
+            }
+        }
     }
 }
